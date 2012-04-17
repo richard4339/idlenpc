31b23b25c257820947e9c97656349e645830525f
Richard <richard@mozor.net>
2012-04-16 22:18:09 -0500

    Updated from zach

--- idlenpc.py
+++ idlenpc.py
@@ -1,61 +1,35 @@
-"""
-    IdleNPC
-	Non-Playable-Characters for IdleRPG
-    
-"""
+#!/usr/bin/env python
 
-# standard libraries
-from datetime import date, datetime, timedelta
-import random, time, sys, re, urllib2, urllib, pprint, json
-from random import choice
+import socket  
+import sys
+import time
+server = "irc.mozor.net" 
+channel = "#idlerpg"
+botnick = sys.argv[1]
+master = "IdleBOT"
 
-# 3rd party modules
-from twisted.internet import protocol, reactor
-from twisted.words.protocols import irc
-from BeautifulSoup import BeautifulSoup
-import pywapi
+def ping(): 
+  ircsock.send("PONG :Pong\n")
 
-class Bot(irc.IRCClient):
+def joinchan(chan):
+  ircsock.send("JOIN "+ chan +"\n")
 
-    def _get_nickname(self):
-        return self.factory.nickname
-    nickname = property(_get_nickname)
+def id(master): 
+  ircsock.send("PRIVMSG "+ master +" :LOGIN "+ botnick +" examplepass\n")
 
-    def signedOn(self):
-        self.join(self.factory.channel)
-        print "Signed on as %s." % (self.nickname,)
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((server, 6667)) 
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Mega Python IdleRPG Master\n")
+ircsock.send("NICK "+ botnick +"\n") 
+time.sleep(5)
 
-    def joined(self, channel):
-        print "Joined %s." % (channel,)
+joinchan(channel) 
+id(master)
 
-    def privmsg(self, user, channel, msg):
-        """ need to pull all of this crap into its own function/class """
-        t = datetime.now()
-        formatted_time = t.strftime("%I:%M:%S")
+while 1: 
+  ircmsg = ircsock.recv(2048) 
+  ircmsg = ircmsg.strip('\n\r')
+  print(ircmsg)
 
-        user = "%s" % (user.split('!', 1)[0].strip(), )
-        if msg[0] == '~':
-            try:
-                command = msg[1:].lower().split()[0]
-                arguments = msg[1:].lower().split()[1:]
-            except:
-                self.msg(self.factory.channel, 'Invalid arguments')
-                return
- 
-class BotFactory(protocol.ClientFactory):
-    protocol = Bot
-
-    def __init__(self, channel, nickname='o_Q'):
-        self.channel = channel
-        self.nickname = nickname
-
-    def clientConnectionLost(self, connector, reason):
-        print "Lost connection (%s), reconnecting." % (reason,)
-        connector.connect()
-
-    def clientConnectionFailed(self, connector, reason):
-        print "Could not connect: %s" % (reason,)
-
-if __name__ == "__main__":
-    reactor.connectTCP('irc.mozor.net', 6667, BotFactory('#mozor', 'o_Q'))
-    reactor.run()
+  if ircmsg.find("PING :") != -1:
+    ping()
