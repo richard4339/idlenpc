090d3388d6c111152069f7b7afcd9a0d3f342235
Richard <richard@mozor.net>
2012-04-18 18:44:44 -0500

    Updated to use a config file

--- idlenpc.py
+++ idlenpc.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+# IdleNPC
+# https://github.com/mozor/idlenpc
+
 import socket  
 import sys
 import time
-server = "irc.mozor.net" 
-channel = "#idlerpg"
-botnick = sys.argv[1]
-master = "IdleBOT"
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read('idlenpc.cfg')
+
+server = config.get('IRC', 'server')
+port = config.get('IRC', 'port')
+channel = config.get('IRC', 'channel')
+botnick = config.get('IRC', 'nickname')
+master = config.get('IRC', 'idlebot')
+char_class = config.get('IRC', 'character_class')
+host = config.get('Network', 'local_ip')
 
 def ping(): 
   ircsock.send("PONG :Pong\n")
@@ -14,17 +25,24 @@ def ping():
 def joinchan(chan):
   ircsock.send("JOIN "+ chan +"\n")
 
+def register(chan):
+	ircsock.send("PRIVMSG "+ master +" :REGISTER "+ botnick +" examplepass " + char_class + "\n")
+
 def id(master): 
   ircsock.send("PRIVMSG "+ master +" :LOGIN "+ botnick +" examplepass\n")
 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667)) 
+ircsock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+ircsock.bind((host, 0))
+ircsock.connect((server, port)) 
 ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Mega Python IdleRPG Master\n")
 ircsock.send("NICK "+ botnick +"\n") 
 time.sleep(5)
 
 joinchan(channel) 
 id(master)
+
+#if sys.argv[2] == "register":
+#	register(channel)
 
 while 1: 
   ircmsg = ircsock.recv(2048) 
